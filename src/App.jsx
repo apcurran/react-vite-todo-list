b94e30b4e8632be91252af26e674e697e7eb954a
Alex Curran <alexcurran.va@gmail.com>
2024-04-26 13:28:57 -0500

    Add local storage for persistent todos data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import NewTodoForm from "./components/new-todo-form/NewTodoForm";
 import TodosList from "./components/todos-list/TodosList";
 
 function App() {
-    const [myTodos, setMyTodos] = useState([]);
+    const [myTodos, setMyTodos] = useState(() => {
+        return JSON.parse(localStorage.getItem("myTodos")) || [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem("myTodos", JSON.stringify(myTodos));
+    }, [myTodos]);
 
     return (
         <div className="wrapper">
