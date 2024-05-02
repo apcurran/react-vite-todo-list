2ce0ebc5ec8551fa9699db9f39f9a28d29b838dd
Alex Curran <alexcurran.va@gmail.com>
2024-05-02 14:39:32 -0500

    Test for deleting a todo

--- cypress/e2e/todo-add-spec.cy.js
+++ cypress/e2e/todo-add-spec.cy.js
@@ -1,5 +1,6 @@
 describe("Add Todo", () => {
     beforeEach(() => {
+        // go to todo app page
         cy.visit("/");
         // add todo before each test
         cy.contains("New Todo").should("be.visible");
@@ -17,5 +18,11 @@ describe("Add Todo", () => {
         cy.get(".popover-form").find("input").type("Walk the cat");
         cy.get(".popover-form").find("button").click();
         cy.contains("Walk the cat").should("be.visible");
+    });
+
+    it("Deletes 'Walk the dog' todo", () => {
+        cy.contains("Walk the dog").should("be.visible");
+        cy.contains("Delete").click();
+        cy.contains("Walk the dog").should("not.exist");
     });
 });
