3861580ecaebb5dd5da88aa633a677421d741ca0
Alex Curran <alexcurran.va@gmail.com>
2024-05-03 14:23:19 -0500

    Adjust config for setup with Component testing

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
     e2e: {
-        baseUrl: "http://localhost:5173/" // URL of Vite react todo app
-    }
+        baseUrl: "http://localhost:5173/", // URL of Vite react todo app
+    },
+    component: {
+        devServer: {
+            framework: "react",
+            bundler: "vite",
+        },
+    },
 });
